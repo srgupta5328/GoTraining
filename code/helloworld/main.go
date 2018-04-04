1ff79cba585e28432de14c3d1299776626134068
Rohan Gupta <rohangupta@Rohans-MacBook-Pro.local>
2018-04-04 10:30:15 -0700

    Hello World Go Program

---
+++ code/helloworld/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi Rohan! You just ran your first go program")
+}
+
+//Side Notes:
+// go build -> compiles a bunch of go source code files
+// go run -> Compiles program
+// go fmt -> formats all the code in each file in the current directory
+// go install compiles and installs a package
+// go get Downlaods the raw source of someone elses package
