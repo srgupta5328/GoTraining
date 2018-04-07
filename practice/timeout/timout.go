a0a141e4c439502af3aa50ebbde9d0506ee2edec
Rohan Gupta <rohangupta@Rohans-MacBook-Pro.local>
2018-04-06 17:04:04 -0700

    added a few more sections timeout channels etc.

---
+++ practice/timeout/timout.go
@@ -0,0 +1,19 @@
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout 1")
+	}
+}
