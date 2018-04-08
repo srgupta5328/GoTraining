5aaa1e72cff38c67476161801befa12afff8090a
Rohan Gupta <rohangupta@Rohans-MacBook-Pro.local>
2018-04-08 12:56:19 -0700

    sending more updates

--- code/cards/main.go
+++ code/cards/main.go
@@ -1,12 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	card := newCard()
-	fmt.Println(card)
-}
+	cards := newDeck()
 
-func newCard() string {
-	return "Five of Daimonds"
+	cards.print()
 }
